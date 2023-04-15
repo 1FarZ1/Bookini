70e4f9f4a4ee3940422e46cc9cc29431f6533411
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-16 01:05:44 +0200

    v3

--- lib/core/consts.dart
+++ lib/core/consts.dart
@@ -1,3 +1,44 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+
+
 class AppConsts {
-  
+  static Color kPrimaryColor = const Color(0xff100B20);
+  static Duration kTranstionDuration = const Duration(milliseconds: 250);
+  static String kGtSectraFine = 'GT Sectra Fine';
 }
+
+
+class AssetsData {
+  static const logo = 'assets/images/Logo.png';
+  static const testImage = 'assets/images/test_image.png';
+}
+
+
+abstract class Styles {
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static  TextStyle textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w900,
+    fontFamily: AppConsts.kGtSectraFine,
+    letterSpacing: 1.2,
+  );
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+}
+
+const TextStyle textStyle = TextStyle();
