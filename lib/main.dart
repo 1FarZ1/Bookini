ef0b222ebdff61f260fa45290160da06d0d793af
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-16 00:51:56 +0200

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,75 +19,11 @@ class MyApp extends StatelessWidget {
 
 class Home extends StatelessWidget {
   Home({super.key});
-  // bool isIncremented = false;
 
   @override
   Widget build(BuildContext context) {
-    // final items = context.watch<TodoCubit>().names;
     return Scaffold(
-      body: ListView.builder(
-        // itemCount: items.length,
-        itemBuilder: (_, index) {
-          return ListTile(
-            contentPadding: EdgeInsets.only(left: 80, bottom: 20),
-            leading: Icon(Icons.circle),
-            title: Text("index"),
-          );
-        },
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              // context.read<TodoCubit>().addTodo(name: 'Fares');
-            },
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              // context.read<TodoCubit>().removeTodo(index: items.length - 1);
-            },
-            child: const Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              // context.read<TodoCubit>().reset();
-            },
-            child: const Icon(Icons.refresh),
-          ),
-        ],
-      ),
+      body: SizedBox.shrink()
     );
   }
 }
-
-
-//  Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               Text(
-//                 state.val.toString(),
-//                 style: const TextStyle(fontSize: 50),
-//               ),
-//               Visibility(
-//                 visible: isIncremented,
-//                 child: const Text(
-//                   'Increamented',
-//                 ),
-//               ),
-//               const SizedBox(
-//                 width: double.infinity,
-//               )
-//             ],
-//           );
-
-  // listenWhen: (previous, current) {
-        //   if (current.val > previous.val) {
-        //     isIncremented = true;
-        //     return true;
-        //   } else {
-        //     isIncremented = false;
-        //     return false;
-        //   }
-        // },
