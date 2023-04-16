c35223202811e15b0ff68ae4b8bb498bbf01c51b
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-16 09:42:57 +0200

    best seller section

--- lib/view/home/widgets/home_body.dart
+++ lib/view/home/widgets/home_body.dart
@@ -103,38 +103,53 @@ class BestSellerBookCard extends StatelessWidget {
           const SizedBox(
             width: 10,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.height * .25,
-                child: Text(
-                  "Harry Potter and the Goblet of Fire",
-                  style:
-                      Styles.textStyle20.copyWith(fontWeight: FontWeight.w700),
-                ),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              const Text(
-                "JK Rowling",
-                style: Styles.textStyle14,
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Row(
-                children: [
-                  Text(
-                    "19,19 \$ ",
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.height * .25,
+                  child: Text(
+                    "Harry Potter and the Goblet of Fire",
                     style: Styles.textStyle20
                         .copyWith(fontWeight: FontWeight.w700),
                   ),
-                  Text(""),
-                ],
-              )
-            ],
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text(
+                  "JK Rowling",
+                  style: Styles.textStyle14,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "19,19 \$ ",
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.w700),
+                    ),
+                    Row(
+                      children: const [
+                        Icon(Icons.star, color: Colors.yellow),
+                        SizedBox(
+                          width: 1,
+                        ),
+                        Text("4.5"),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text("(100)"),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
