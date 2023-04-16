2a79a2cbf3a1fa0ff94532a530a6545290236792
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-16 03:23:18 +0200

    adding animation

--- lib/view/splash/widgets/splash_body.dart
+++ lib/view/splash/widgets/splash_body.dart
@@ -1,8 +1,35 @@
 import 'package:bookini/core/consts.dart';
 import 'package:flutter/material.dart';
 
-class SplashBody extends StatelessWidget {
+class SplashBody extends StatefulWidget {
   const SplashBody({super.key});
+
+  @override
+  State<SplashBody> createState() => _SplashBodyState();
+}
+
+class _SplashBodyState extends State<SplashBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<Offset> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = Tween<Offset>(
+      begin: const Offset(0, 3),
+      end: Offset.zero,
+    ).animate(_animationController);
+    _animationController.forward();
+    _animation.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +38,16 @@ class SplashBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsData.logo),
-        const Text("World Of Books",
-            textAlign: TextAlign.center, style: Styles.textStyle14),
+        AnimatedBuilder(
+          builder: (ctx, _) {
+            return SlideTransition(
+              position: _animation,
+              child: const Text("World Of Books",
+                  textAlign: TextAlign.center, style: Styles.textStyle14),
+            );
+          },
+          animation: _animation,
+        ),
       ],
     );
   }
