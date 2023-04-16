26c44ce901df32bf48078b1567ec42bbafa5f2c4
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-16 06:56:52 +0200

    list view added

--- lib/view/home/widgets/home_body.dart
+++ lib/view/home/widgets/home_body.dart
@@ -8,7 +8,19 @@ class HomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomListView();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          CustomListView(),
+          Text(
+            "Best Seller",
+            style: Styles.textStyle18,
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -19,15 +31,21 @@ class CustomListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .35,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-            // width: MediaQuery.of(context).size.width * .55,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    image: AssetImage(AssetsData.testImage),
-                    fit: BoxFit.fill))),
+      child: ListView.builder(
+        itemCount: 5,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return AspectRatio(
+            aspectRatio: 2.7 / 4,
+            child: Container(
+                // width: MediaQuery.of(context).size.width * .55,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    image: const DecorationImage(
+                        image: AssetImage(AssetsData.testImage),
+                        fit: BoxFit.fill))),
+          );
+        },
       ),
     );
   }
