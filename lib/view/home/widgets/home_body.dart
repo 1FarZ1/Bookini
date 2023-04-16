1a866faa6242f4934d33d0c526357f49dbc4914b
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-04-16 09:14:25 +0200

    adding font

--- lib/view/home/widgets/home_body.dart
+++ lib/view/home/widgets/home_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookini/core/consts.dart';
 import 'package:flutter/material.dart';
+
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
@@ -14,7 +15,9 @@ class HomeBody extends StatelessWidget {
           Text(
             "Best Seller",
             style: Styles.textStyle18,
-          )
+          ),
+          SizedBox(height: 20),
+          ListViewBooks()
         ],
       ),
     );
@@ -56,6 +59,84 @@ class BookCard extends StatelessWidget {
             fit: BoxFit.fill,
           ),
         ),
+      ),
+    );
+  }
+}
+
+class ListViewBooks extends StatelessWidget {
+  const ListViewBooks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return const BestSellerBookCard();
+        },
+      ),
+    );
+  }
+}
+
+class BestSellerBookCard extends StatelessWidget {
+  const BestSellerBookCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 140,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 3.2 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14),
+                image: const DecorationImage(
+                  image: AssetImage(AssetsData.testImage),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.height * .25,
+                child: Text(
+                  "Harry Potter and the Goblet of Fire",
+                  style:
+                      Styles.textStyle20.copyWith(fontWeight: FontWeight.w700),
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              const Text(
+                "JK Rowling",
+                style: Styles.textStyle14,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "19,19 \$ ",
+                    style: Styles.textStyle20
+                        .copyWith(fontWeight: FontWeight.w700),
+                  ),
+                  Text(""),
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
